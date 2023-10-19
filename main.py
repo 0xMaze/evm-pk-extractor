d6e73ce085b8f1a98deb7489be4f37b8aa2f67a9
maze <zaharmalinovskijj@gmail.com>
2023-10-20 01:02:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from web3 import Web3, Account
+
+
+class Extractor:
+    def __init__(self) -> None:
+        Account.enable_unaudited_hdwallet_features()
+
+    def extract(self) -> bool:
+        private_keys = []
+        seed_phrases = Extractor.read_from_txt()
+        for seed in seed_phrases:
+            account = Web3().eth.account.from_mnemonic(mnemonic=seed)
+            private_keys.append(f"{account.address} : {account.key.hex()} : {seed}")
+        return Extractor.write_strings_to_file(string_array=private_keys)
+
+    @staticmethod
+    def read_from_txt(file_path: str = "seed_phrases.txt") -> list[str]:
+        try:
+            with open(file_path, "r") as file:
+                return [line.strip() for line in file]
+        except FileNotFoundError as e:
+            print(f"File '{file_path}' not found.")
+        except Exception as e:
+            print(f"Encountered an error while reading a TXT file '{file_path}': {e}.")
+
+    @staticmethod
+    def write_strings_to_file(string_array: list[str], file_path: str = "keys.txt") -> bool:
+        try:
+            with open(file_path, "w") as file:
+                for string in string_array:
+                    file.write(string + "\n")
+        except FileNotFoundError as e:
+            print(f"File '{file_path}' not found.")
+        except Exception as e:
+            print(f"Encountered an error while writing to a TXT file '{file_path}': {e}.")
+
+
+def main() -> None:
+    extractor = Extractor()
+    extractor.extract()
+
+
+if __name__ == "__main__":
+    main()
